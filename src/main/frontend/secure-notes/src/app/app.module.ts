a36382252508cecb3a7612f2b28a18806aaa5118
steull6163 <steull6163@users.noreply.github.com>
2022-11-18 13:31:44 +0100

    crypto-js, hybrid-crypto-js und ngx-webstorage hinzugefügt

--- src/main/frontend/secure-notes/src/app/app.module.ts
+++ src/main/frontend/secure-notes/src/app/app.module.ts
@@ -9,23 +9,30 @@ import { AppComponent } from './app.component';
 import { SecurenotesComponent } from './securenotes/securenotes.component';
 import { SecurenotesDialog } from './securenotes-dialog/securenotes-dialog.component';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { EncryptPipe, DecryptPipe } from './crypt/enctypt-decrytp.pipe';
+import { NgxWebstorageModule } from 'ngx-webstorage';
+//import { } from 'rsa-aes-hybrid';
 
 @NgModule({
   declarations: [
     AppComponent,
     SecurenotesComponent,
-    SecurenotesDialog
+    SecurenotesDialog,
   ],
   imports: [
     AppRoutingModule,
     AppMaterialModule,
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule,
+    NgxWebstorageModule.forRoot(),
     ReactiveFormsModule
   ],
-  providers: [],
+  exports: [
+    
+  ],
+  providers: [ EncryptPipe, DecryptPipe ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
