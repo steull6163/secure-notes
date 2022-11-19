6952c90253e7344bf5a33ac4759abc64101d3fce
steull6163 <steull6163@users.noreply.github.com>
2022-11-19 21:45:54 +0100

    SecureNotesRepository#findByTitle hinzugefügt, delete gibt boolean zurück

--- src/main/java/de/stullrich/securenotes/repository/SecureNotesRepository.java
+++ src/main/java/de/stullrich/securenotes/repository/SecureNotesRepository.java
@@ -2,7 +2,13 @@ package de.stullrich.securenotes.repository;
 
 import de.stullrich.securenotes.model.SecureNote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface SecureNotesRepository extends JpaRepository<SecureNote, Long> {
 
+	@Query("SELECT s from SecureNote s WHERE s.title = :title")
+	public Optional<SecureNote> findByTitle(@Param("title") String title);
 }
